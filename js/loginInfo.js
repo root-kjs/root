1e330a6acd374bdd0485997c874a399407afbfd2
root-kjs <root.kjs82@gmail.com>
2025-07-04 15:11:40 +0900

    로그아웃에러수정

--- js/loginInfo.js
+++ js/loginInfo.js
@@ -23,6 +23,15 @@ function userInfo(  ){   console.log( '!회원정보 출력함수 >>> userInfo( 
     let loginUser = getLoginInfo(); // 세션스토리지 로그인 회원정보 가져오기
     console.log( loginUser.loginId );
 
+    if( loginUser.loginId == undefined ){
+            let mainText =document.querySelector('.mainText .green');
+    let loginUserName = document.querySelector('.memberMenu li:nth-child(1)');
+    let loginUserOut = document.querySelector('.memberMenu li:nth-child(2)');
+
+    mainText.innerHTML = ` <  더조은 수강생님, R&D 협력자 모집중  > `;
+    loginUserName.innerHTML = `<a href="./member/login.html">로그인</a>`;
+    loginUserOut.innerHTML = `<a href="./member/signUp.html">회원가입</a>`;
+    }else{
     let mainText =document.querySelector('.mainText .green');
     let loginUserName = document.querySelector('.memberMenu li:nth-child(1)');
     let loginUserOut = document.querySelector('.memberMenu li:nth-child(2)');
@@ -30,6 +39,9 @@ function userInfo(  ){   console.log( '!회원정보 출력함수 >>> userInfo( 
     mainText.innerHTML = `<span style="text-decoration:underline"> ${ loginUser.userName } 님</span> <br/> 요즘 사업은 어떠신가요? `;
     loginUserName.innerHTML = `<a href="#">${ loginUser.userName }( ${ loginUser.loginId } )</a>`;
     loginUserOut.innerHTML = ` <a href="#" onclick="logOut()">로그아웃</a> `;
+
+    }
+
 
        
 
